d25c1a6b06ce0cc85335517aa3b17ccd4b9719fd
zeng715734199 <715734199@qq.com>
2021-07-29 12:42:25 +0800

    MVC 模块化

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -11437,7 +11437,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52314" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51509" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
